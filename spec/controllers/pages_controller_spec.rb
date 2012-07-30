3353a34039bc97a61fb14b3c161cc7c7f92adb87
thlias <thlias2000@gmail.com>
2012-07-30 10:29:42 +0100

    put some more tests in rspect and text in pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,6 +15,11 @@ describe PagesController do
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample app | Home")
     end
     
+    it "should have a non-blank body" do
+      get "home"
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+    
   end
 
   describe "GET 'contact'" do
@@ -22,12 +27,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get "contact"
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample app | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get "about"
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample app | About")
     end
   end
 
